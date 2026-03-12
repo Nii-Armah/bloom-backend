10b24a7774e65674199bc61e7323b746a3885e35
Nii-Armah <stanleyniiarmah7@gmail.com>
2026-03-12 14:50:52 +0000

    Create ErrorCode
    Handles management of machine-readable response error codes across the project.

---
+++ constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class ErrorCode(str, Enum):
+    VALIDATION_ERROR = 'VALIDATION_ERROR'
+    NOT_FOUND = 'NOT_FOUND'
+    CONFLICT = 'CONFLICT'
+    AUTH_FAILED = 'AUTH_FAILED'
